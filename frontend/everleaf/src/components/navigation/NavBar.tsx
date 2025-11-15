6248438073154accd5b90e37fdc9115050a718a3
External Host <haydenr261@gmail.com>
2025-11-15 13:26:29 -0800

    globe position

--- frontend/everleaf/src/components/navigation/NavBar.tsx
+++ frontend/everleaf/src/components/navigation/NavBar.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import MainButton from "../utils/MainButton";
 import UserMainButtonProp from "../utils/UserMainButtonProp";
+import { IoMdLeaf } from "react-icons/io";
 
 const NavBar = () => {
 
@@ -10,9 +11,10 @@ const NavBar = () => {
 
     return (
         <>
-        <div className="fixed absolute top-0 w-full h-[100px] border border-white backdrop-blur-lg bg-white/10">
+        <div className="fixed top-0 w-full h-[100px] border border-white backdrop-blur-lg bg-white/10">
         <div className="flex flex-cols h-full items-center">
             <div className="flex w-full p-5 gap-x-[2%] justify-left">
+                <a href="/"><IoMdLeaf className="text-5xl"/></a>
                 <MainButton label="Home" onClick={() => {router.push("/")}}/>
                 <MainButton label="Our Mission" onClick={() => {router.push("/mission")}}/>
                 <MainButton label="Home" onClick={() => {router.push("/")}}/>
---
+++ frontend/everleaf/src/components/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+import Globe from "../misc/Globe";
+import NavBar from "../navigation/NavBar";
+
+export default function HomePage() {
+    return (
+        <>
+            <div className="">
+                <NavBar />
+                <div className="relative h-screen overflow-hidden">
+                    <div className="absolute translate-y-6 top-0 -right-32 -z-10 pointer-events-none">
+                        <Globe />
+                    </div>
+
+                    <div className="flex items-center h-full pl-20">
+                        <div>
+                            <h1 className="text-6xl">Everleaf</h1>
+                            <h5>Some short description</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
