b0e187b59b95a7fdba5d56a70a4a1e181dd66366
External Host <haydenr261@gmail.com>
2025-11-15 11:03:37 -0800

    stuff

--- frontend/everleaf/src/components/DarkModeSwitch.tsx
+++ frontend/everleaf/src/components/DarkModeSwitch.tsx
@@ -1,9 +1,30 @@
+'use client';
 
+import { useEffect, useState } from "react"
+import { LuSunDim, LuMoon } from "react-icons/lu";
 
 export default function DarkModeSwitch() {
+    const [darkMode, setdarkMode] = useState<boolean>(false)
+
+    // load local item
+    useEffect(() => {
+        const json = localStorage.getItem("isDarkmode");
+        if (json) setdarkMode(JSON.parse(json));
+    }, []);
+
+    // on change, do this
+    useEffect(() => {
+        document.documentElement.classList.toggle("dark", darkMode);
+        localStorage.setItem("isDarkMode", darkMode.toString());
+    }, [darkMode]);
+
     return (
         <div>
-            <h1>SWITCH</h1>
+            {
+                (darkMode)
+                ? <div><LuSunDim/></div>
+                : <div><LuMoon/></div>
+            }
         </div>
     )
 };
