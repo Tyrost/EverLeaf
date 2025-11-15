8744708312319c1f42543429b769a4409377f855
Tyrost <ccorzo306@gmail.com>
2025-11-15 11:53:25 -0800

    Progressed on NaBar

---
+++ frontend/everleaf/src/components/navigation/NavBar.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import MainButton from "../utils/MainButton";
+import UserMainButtonProp from "../utils/UserMainButtonProp";
+
+const NavBar = () => {
+
+    const router = useRouter()
+
+    return (
+        <>
+        <div className="fixed absolute top-0 w-full h-[100px] border border-white backdrop-blur-lg bg-white/10">
+        <div className="flex flex-cols h-full items-center">
+            <div className="flex w-full p-5 gap-x-[2%] justify-left">
+                <MainButton label="Home" onClick={() => {router.push("/")}}/>
+                <MainButton label="Our Mission" onClick={() => {router.push("/mission")}}/>
+                <MainButton label="Home" onClick={() => {router.push("/")}}/>
+                <MainButton label="Home" onClick={() => {router.push("/")}}/>
+            </div>
+            <div>
+                <UserMainButtonProp/>
+            </div>
+        </div>
+
+
+
+        </div>
+        </>
+    );
+}
+
+export default NavBar;
---
+++ frontend/everleaf/src/components/utils/UserMainButtonProp.tsx
@@ -0,0 +1,16 @@
+
+
+const UserMainButtonProp = () => {
+
+
+    return (
+        <>
+        <div className="w-[50px] h-[50px] border border-white rounded-full mx-10">
+
+
+        </div>
+        </>
+    );
+}
+
+export default UserMainButtonProp;
