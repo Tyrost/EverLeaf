787a24335e477b47ef3a38f9c853a19c24c3bafe
Song <songzhang015@gmail.com>
2025-11-15 10:20:21 -0800

    Add backend starter app.py, requirements.txt, new .gitignore

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import certifi
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
+db = client["db"]
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
